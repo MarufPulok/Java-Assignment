1bc62f3a9008255ebf5244b63e15ebd27209dc78
MarufPulok <53114706+MarufPulok@users.noreply.github.com>
2021-02-01 14:05:15 +0600

    Add files via upload

---
+++ BasicJava/src/basicjava/BioData.java
@@ -0,0 +1,14 @@
+
+package basicjava;
+
+
+public class BioData {
+    public static void main(String[] args){
+    
+    System.out.println("Hello World");
+    System.out.println("Hello World");
+    int a=90;
+    System.out.printf("%d\n",a);
+    }
+            
+}
